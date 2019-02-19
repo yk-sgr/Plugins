760e32574aacb60e14d567930c83de6c629d6307
ForYaSee <foryasee@gmail.com>
2019-02-19 14:06:12 +0100

    1.0.1 (#4)
    * Resolves #1 (#2)
    
    * Add codeclimate badge
    
    * Sonatype

--- src/main/java/de/foryasee/plugins/loader/PluginLoadedResult.java
+++ src/main/java/de/foryasee/plugins/loader/PluginLoadedResult.java
@@ -4,6 +4,7 @@ import de.foryasee.plugins.Plugin;
 
 /**
  * Represents the result of a loading action.
+ *
  * @param <T> the type of the {@link Plugin} implementation.
  */
 public class PluginLoadedResult<T extends Plugin> {
@@ -11,7 +12,7 @@ public class PluginLoadedResult<T extends Plugin> {
   private final T plugin;
   private final Throwable throwable;
 
-  public PluginLoadedResult(T plugin, Throwable throwable) {
+  public PluginLoadedResult(final T plugin, final Throwable throwable) {
     this.plugin = plugin;
     this.throwable = throwable;
   }
